04ca87a13c649b0f31a49e7fde80e8a76b5cac4d
Raphael Gabrig <raphael.gabrig@hotmail.com>
2025-02-15 11:06:04 -0300

    Consuming Redis Queues

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,25 +1,55 @@
 import { test, expect } from '@playwright/test';
 
+import { obterCodigo2FA } from '../support/db';
+
+import { LoginPage } from '../pages/LoginPage';
+import { DashPage } from '../pages/DashPage';
+
+import { cleanJobs, getJob } from '../support/redis';
+
 test('Nao deve logar quando o codigo de autenticacao e invalido', async ({ page }) => {
+  const loginPage = new LoginPage(page)
   
+
   const usuario = {
     cpf: '00000014141',
     senha: '147258'
   }
 
-  await page.goto('http://paybank-mf-auth:3000/');
-  
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
+  await loginPage.acessaPagina()
+  await loginPage.informaCpf(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
+  await loginPage.informa2FA('123456')
 
-  for (const digito of usuario.senha) {
-    await page.getByRole('button', { name: digito }).click();
+  expect(await page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+});
+
+test('Deve acessar a conta do usuario', async ({ page }) => {
+  
+  const loginPage = new LoginPage(page)
+  const dashPage = new DashPage(page)
+
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258'
   }
 
-  await page.getByRole('button', { name: 'Continuar' }).click();
+  await cleanJobs()
 
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
+  await loginPage.acessaPagina()
+  await loginPage.informaCpf(usuario.cpf)
+  await loginPage.informaSenha(usuario.senha)
 
-  await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+  //checkpoint
+  await page.getByRole('heading', {name: 'Verificação em duas etapas'})
+    .waitFor({timeout: 3000})
+
+  const codigo = await getJob()
+
+  // const codigo = await obterCodigo2FA(usuario.cpf)
+
+  await loginPage.informa2FA(codigo)
+
+  await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
+  
 });
